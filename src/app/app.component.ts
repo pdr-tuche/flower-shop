8a85a88f293a86213fab500875a4a298794a46be
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-09-29 14:30:37 -0300

    refactor: componentizado seção home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header/header.component";
+import { HomeSectionComponent } from "./shared/components/home-section/home-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HomeSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
