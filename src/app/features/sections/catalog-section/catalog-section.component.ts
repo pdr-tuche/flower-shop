3aedacc43fc71ba00bf578972df8d31833fbfd32
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-10-02 23:46:12 -0300

    feat: criado html catalog-section

--- src/app/features/sections/catalog-section/catalog-section.component.ts
+++ src/app/features/sections/catalog-section/catalog-section.component.ts
@@ -5,8 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './catalog-section.component.html',
-  styleUrl: './catalog-section.component.scss'
+  styleUrl: './catalog-section.component.scss',
 })
 export class CatalogSectionComponent {
-
+  catalog = [
+    {
+      product: 'Scarlet Symphony',
+      price: 30.0,
+      image: 'images/Photo - 1.png',
+    },
+    {
+      product: 'Azure Serenade',
+      price: 35.0,
+      image: 'images/Photo - 2.png',
+    },
+    {
+      product: 'Rosy Blossoms',
+      price: 50.0,
+      image: 'images/Photo - 3.png',
+    },
+    {
+      product: 'Spring Bouquet',
+      price: 70.0,
+      image: 'images/Photo - 4.png',
+    },
+  ];
 }
