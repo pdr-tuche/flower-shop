55ae2726b3bc3849ae2916b49cd27648257976dd
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-10-02 23:29:12 -0300

    feat: criado secao catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HomeSectionComponent } from './features/sections/home-section/home-section.component';
 import { AboutUsSectionComponent } from './features/sections/about-us-section/about-us-section.component';
 import { ServicesSectionComponent } from './features/sections/services-section/services-section.component';
+import { CatalogSectionComponent } from './features/sections/catalog-section/catalog-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeSectionComponent, AboutUsSectionComponent, ServicesSectionComponent],
+  imports: [
+    RouterOutlet,
+    HomeSectionComponent,
+    AboutUsSectionComponent,
+    ServicesSectionComponent,
+    CatalogSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/features/sections/catalog-section/catalog-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog-section.component.html',
+  styleUrl: './catalog-section.component.scss'
+})
+export class CatalogSectionComponent {
+
+}
